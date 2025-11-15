ba566e1f68c2c6f835e7996c64740670a553a5ab
Uzair-dot-rgb <muzair2012@outlook.com>
2025-11-15 10:17:46 -0500

    acp

---
+++ acp.py
@@ -0,0 +1,11 @@
+my_string1 = 42
+my_string2 = 21
+my_string3 = 11
+
+new_string1 = my_string1.replace("42", "99")
+new_string2 = my_string2.replace("21", "1")
+new_string3 = my_string3.replace("11", "67")
+
+print(new_string1)
+print(new_string2)
+print(new_string3)
